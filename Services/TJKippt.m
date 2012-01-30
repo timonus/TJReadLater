d26579dd03a004ca9f56be1cc8b2a0b16c8e0302
Tim Johnsen <timonus@gmail.com>
2012-01-29 16:55:38 -0800

    Kippt URL saving up and running

--- Services/TJKippt.m
+++ Services/TJKippt.m
@@ -24,9 +24,7 @@
 #pragma mark Private
 
 + (NSURLRequest *)_requestForAuthWithUsername:(NSString *)username password:(NSString *)password {
-	NSMutableURLRequest *request = nil;
-	
-	request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://kippt.com/api/v0/account/verify/"]]];
+	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://kippt.com/api/v0/account/verify/"]]];
 	[request setValue:[NSString stringWithFormat:@"Basic %@", [[[NSString stringWithFormat:@"%@:%@", username, password] dataUsingEncoding:NSUTF8StringEncoding] base64EncodedString]] forHTTPHeaderField:@"Authorization"];
 	
 	[request setHTTPMethod:@"GET"];
@@ -35,9 +33,27 @@
 }
 
 + (NSURLRequest *)_requestForSaveURL:(NSString *)url title:(NSString *)title {
-	NSMutableURLRequest *request = nil;
+	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://kippt.com/api/v0/clips/"]]];
+	[request setHTTPMethod:@"POST"];
+	[request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+	
+	// Message body
+	NSMutableString *body = [NSMutableString stringWithFormat:@"{ \"url\": \"%@\"", url];
+	if (title) {
+		[body appendFormat:@", \"title\": \"%@\"", title];
+	}
+	[body appendString:@" }"];
+	
+	[request setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
+
+	// Auth
+	[request setValue:[NSString stringWithFormat:@"Basic %@", [[[NSString stringWithFormat:@"%@:%@", [[NSUserDefaults standardUserDefaults] objectForKey:[NSString stringWithFormat:@"%@Username", NSStringFromClass(self)]], [[NSUserDefaults standardUserDefaults] objectForKey:[NSString stringWithFormat:@"%@Password", NSStringFromClass(self)]]] dataUsingEncoding:NSUTF8StringEncoding] base64EncodedString]] forHTTPHeaderField:@"Authorization"];
+	
 	return request;
 }
 
++ (int)_saveSuccessCode {
+	return 201;
+}
 
 @end
