f8031b4de841fad56eada3bfa9d381b8d67ba454
Chih-Wei Lee <dlackty@gmail.com>
2012-02-07 03:46:59 +0800

    Fix a typo in config file.

--- TJConfig.h
+++ TJConfig.h
@@ -4,4 +4,4 @@
 // Read It Later - http://readitlaterlist.com/api/signup/
 
 // #define READ_IT_LATER_API_KEY @"<Your API Key>"
-// #define READ_IT_LATER_APP_NAME @"<READ_IT_LATER_APP_NAME>"
+// #define READ_IT_LATER_APP_NAME @"<Your App Name>"
