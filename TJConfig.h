176ac2407ecccccad8dc26694dc10892728ec1c8
Chih-Wei Lee <dlackty@gmail.com>
2012-02-07 03:40:04 +0800

    Move Read It Later credentials into a separate config file.

---
+++ TJConfig.h
@@ -0,0 +1,7 @@
+// TJReadLater
+// By Tim Johnsen
+
+// Read It Later - http://readitlaterlist.com/api/signup/
+
+// #define READ_IT_LATER_API_KEY @"<Your API Key>"
+// #define READ_IT_LATER_APP_NAME @"<READ_IT_LATER_APP_NAME>"
