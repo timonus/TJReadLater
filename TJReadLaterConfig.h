5b9221abaf16920d6f193e6a19faaf2001c8dd22
Tim Johnsen <timonus@gmail.com>
2012-02-09 23:48:29 -0800

    Added TJReadLaterConfig for keys

---
+++ TJReadLaterConfig.h
@@ -0,0 +1,9 @@
+// TJReadLaterConfig
+// By Tim Johnsen
+
+#pragma mark -
+#pragma mark Read It Later
+
+#warning Missing Read It Later Credentials
+const NSString *kTJReadLaterReadItLaterAPIKey = @"";
+const NSString *kTJReadLaterReadItLaterAppName = @"";
